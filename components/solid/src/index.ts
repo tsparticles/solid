03eb08a39898b4bcd5e99be7c3cc952bde844771
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-12-29 16:02:46 +0100

    build: init repository

---
+++ components/solid/src/index.ts
@@ -0,0 +1,8 @@
+import Particles from "./Particles";
+import type { IParticlesProps } from "./IParticlesProps";
+
+export type { IParticlesProps };
+export type ParticlesProps = IParticlesProps;
+
+export default Particles;
+export { Particles };
